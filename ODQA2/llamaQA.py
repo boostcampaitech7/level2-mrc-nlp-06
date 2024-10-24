3b2abd952b9b1fbc4ac4367b1f3b59d5bc6f8164
minjijeong98 <minji.jeong.ds@gmail.com>
2024-10-24 23:14:48 +0900

    fix: Update odqa process according to new llama reader code
    #27

--- ODQA2/llamaQA.py
+++ ODQA2/llamaQA.py
@@ -26,6 +26,8 @@ class LlamaQA():
         with open(config_path, "r") as f:
             config = json.load(f)
         self.args = Box(config)
+        with open(self.args.propmt_path, "r") as f:
+            self.prompt_template = f.read()
         self.tokenizer = None 
         self.model = None
         self.model_pipe = None
@@ -35,10 +37,6 @@ class LlamaQA():
         self.tokenizer = AutoTokenizer.from_pretrained(self.args.model_name)
         self.tokenizer.padding_side = "left"
         self.tokenizer.pad_token = self.tokenizer.eos_token
-
-        with open(self.args.chat_template_path, 'r') as file:
-            chat_template = file.read()
-        self.tokenizer.chat_template = chat_template
 
         # load the base model
         bnb_config = BitsAndBytesConfig(
@@ -58,6 +56,7 @@ class LlamaQA():
         self.model.enable_adapters() 
 
         self.model.config.pad_token_id = self.tokenizer.pad_token_id
+        self.model = prepare_model_for_kbit_training(self.model)
         self.model.config.use_cache = False
 
     # Llama conversation format으로 데이터 변환 
@@ -65,38 +64,23 @@ class LlamaQA():
         # get the question and context for this sample
         question = sample['question']
         context = sample['context']
-
-        # some questions can have multiple answers, some none at all,
-        # for the case of no answers we'll have the model output that the
-        # context does not provide an answer, if it has multiple we'll just take
-        # the first answer as the ground truth.
         answers = sample['answers']['text']
         if len(answers) == 0 :
             answer = "The context does not provide an answer..."
         else:
             answer = sample['answers']['text'][0]
 
-        # now we define an initial model prompt defining the task and giving the model the context passage
-        # 프롬프트 한국어로 바꾸면 성능 더 좋아질까?
-        instruction_prompt_template = '''You are a helpful assistant tasked with extracting passages that answer users questions from a given context. Output exact passages word for word that answer the users question. Do not output any other text other than passages in the context passage. Output the minimal amount to answer the question, for example only 2-3 words from the passage. If you cannot find the answer in the context passage output 'The context does not provide an answer...'
-
-        Context: {context}'''
-
-        # now we'll convert these into a list of messages for our conversation
         messages = [
-            {"role": "system", "content": instruction_prompt_template.format(context=context)},
+            {"role": "system", "content": self.prompt_template.format(context=context)},
             {"role": "user", "content": question},
             {"role": "assistant", "content": answer}
         ]
-        # apply the chat template and return the sample
-        # we'll also return the single answer we expect and the list of messages without
-        # the chat template in case we need them later.
-        sample_conversation = self.tokenizer.apply_chat_template(messages)
+        sample_conversation = self.tokenizer.apply_chat_template(messages, tokenize=False)
         return {"text": sample_conversation, "messages": messages, "answer": answer}
 
     def get_base_and_tuned_bulk_predictions(self, samples):
         bulk_messages = [i[:-1] for i in samples['messages']]
-        responses = self.model_pipe(bulk_messages, max_new_tokens=512, batch_size=len(samples), do_sample=False, top_p=None)
+        responses = self.model_pipe(bulk_messages, max_new_tokens=64, batch_size=len(samples), do_sample=False, top_p=None)
         responses = [i[0]['generated_text'][-1]['content'] for i in responses]
         return {"predicted_answer": responses}
 
@@ -104,14 +88,10 @@ class LlamaQA():
     def predict(self, datasets, save_path):
         # text-generation을 위해 huggingface pipeline으로 모델 감싸기
         self.model_pipe = pipeline("text-generation", model=self.model, tokenizer=self.tokenizer)
-
-        with open(self.args.chat_template_path, 'r') as file:
-            chat_template = file.read()
-        self.tokenizer.chat_template = chat_template
-
+        
         # 데이터 형태 변환 및 추론
         conversation_datasets = datasets.map(self.convert_squad_sample_to_llama_conversation)
-        conversation_datasets = conversation_datasets.map(self.get_base_and_tuned_bulk_predictions, batched=True, batch_size=20)
+        conversation_datasets = conversation_datasets.map(self.get_base_and_tuned_bulk_predictions, batched=True, batch_size=5)
 
         # prediction 결과 저장
         test_result_dict = {}
